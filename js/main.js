9b3aae6bdc044702d3c21175280e82a983b37685
Roman <chv-roman@mail.ru>
2022-02-25 11:56:28 +0300

    Исправлены функции

--- js/main.js
+++ js/main.js
@@ -1,38 +1,25 @@
-function printWarningRandom(callback){
-  if(callback){
-    return 'Вводимое значение должно быть болше либо равно 0';
-  }
-  return 'Число «от» должно быть меньшее, чем число «до» ';
+//функция генерации числа
+function getRandomInt(min, max, quantity){
 
+  return  (Math.random() * (max - min) + min).toFixed(quantity);
 }
-
-function getRandomNumber(firstNumber, secondNumber){
-  if(firstNumber<0 || secondNumber<0){
-    return printWarningRandom(1);
+//функция валидации
+function getRandomValidate(min, max, quantity){
+  if(min>max){
+    const change=min;
+    min=max;
+    max=change;
   }
-  if(secondNumber<=firstNumber){
-    return printWarningRandom();
+  if(min===max){
+    // eslint-disable-next-line no-console
+    console.log('Числа от и до не должны быть равными!');
   }
-  firstNumber = Math.ceil(firstNumber);
-  secondNumber = Math.floor(secondNumber);
-  return Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
+  if(min<0 || max<0){
+    // eslint-disable-next-line no-console
+    console.log('Числа должны быть положительными!');
+  }
+  return getRandomInt(min, max, quantity);
 }
+getRandomValidate();
 
-function getRandomCoordinate(firstNumber, secondNumber, quantity){
-  if(firstNumber<0 || secondNumber<0){
-    return printWarningRandom(1);
-  }
-  if(secondNumber<=firstNumber){
-    return printWarningRandom();
-  }
-  const coordinate = (Math.random() * (secondNumber - firstNumber)) + firstNumber;
-  let result = String(coordinate).split('.');
-  result = `${result[0] }.${ result[1].substring(0, quantity)}`;
-  return Number(result);
-
-}
-
-// Примеры и методы взяты с ссайта developer.mozilla.org
-getRandomCoordinate(110, 100.3, 3);
-
-getRandomNumber(100,100);
+// Примеры и методы взяты с сайта developer.mozilla.org

be7767a8900d518d913f2cfd718a1a6f93aed265
Roman <chv-roman@mail.ru>
2022-03-15 18:48:43 +0300

    Отрисуй меня полностью (часть 1)

--- js/data.js
+++ js/data.js
@@ -1,3 +1,4 @@
+import {getRandomInt, getRandomElement, getRandomValue} from './support.js';
 const HOUSING_TYPES =['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const IN_OUT_TIMES =['12:00', '13:00', '14:00'];
 const FEATURES =['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -10,4 +11,59 @@ const LATITUDE_MAX =35.70000 ;
 const LONGITUDE_MIN = 139.70000;
 const LONGITUDE_MAX = 139.80000;
 const IMAGE_QUANTITY = 10;
-export {HOUSING_TYPES,IN_OUT_TIMES, FEATURES, HOUSING_PHOTOS, LATITUDE_MIN, LATITUDE_MAX, LONGITUDE_MIN, LONGITUDE_MAX, IMAGE_QUANTITY};
+const HOUSING_TYPESRU ={
+  flat:'Квартира',
+  bungalow:'Бунгало',
+  house:'Дом',
+  palace:'Дворец',
+  hotel:'Отель',
+};
+
+
+function createRandomAvatar() {
+  const int = `${getRandomInt(1, IMAGE_QUANTITY)}.png`;
+  const avatar = int.padStart(22, 'img/avatars/user0');
+  return avatar;
+}
+
+function createLocation() {
+  return ({
+    lat: getRandomInt(LATITUDE_MIN, LATITUDE_MAX, 5),
+    lng: getRandomInt(LONGITUDE_MIN, LONGITUDE_MAX, 5),
+  });
+}
+function createAdressString(adress) {
+  return `${adress.lat}, ${adress.lng}`;
+}
+function createAutor() {
+  return ({
+    avatar: createRandomAvatar(),
+  });
+}
+function createOffer() {
+  return ({
+    title: `Объявление ${ getRandomInt(1,10)}`,
+    address: createAdressString(createLocation()),
+    price: getRandomInt(500, 1500),
+    type: HOUSING_TYPESRU[getRandomElement(HOUSING_TYPES)],
+    rooms:getRandomInt(1, 10),
+    guests: getRandomInt(1, 10),
+    checkin: getRandomElement(IN_OUT_TIMES),
+    checkout: getRandomElement(IN_OUT_TIMES),
+    features: getRandomValue(FEATURES),
+    description: 'description',
+    photos: getRandomValue(HOUSING_PHOTOS),
+  });
+}
+
+function createAds() {
+  return (
+    {
+      author: createAutor(),
+      offer: createOffer(),
+      location: createLocation(),
+    });
+};
+
+const getArrayAds = (count)=>Array.from({length: count}, createAds);
+export{getArrayAds};
--- js/main.js
+++ js/main.js
@@ -1,6 +1,1 @@
-import {createAds} from './util.js';
-
-const getArrayAds = Array.from({length: 10}, createAds);
-// eslint-disable-next-line no-console
-console.log(getArrayAds);
-
+import './template.js';
